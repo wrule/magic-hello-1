6594fadef28fc7468f451241473bcdda070c576f
jimao <shihao.gu@perfma.com>
2024-11-13 18:21:18 +0800

    还是有一些问题

--- app/routes/magic.hello.tsx
+++ app/routes/magic.hello.tsx
@@ -1,11 +1,10 @@
 import { LoaderFunction } from '@remix-run/cloudflare';
 import { Jimp, rgbaToInt, loadFont } from 'jimp';
-// import * as Fonts from '@jimp/plugin-print/fonts';
-// import dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
 export const loader: LoaderFunction = async () => {
   const image = new Jimp({ width: 72, height: 21, color: rgbaToInt(0, 20, 255, 255) });
-  // image.print({ font: await loadFont(Fonts.SANS_16_WHITE), x: 3, y: 2, text: dayjs().format('HH:mm:ss') });
+  image.print({ font: await loadFont('public/open-sans-16-white.fnt'), x: 3, y: 2, text: dayjs().format('HH:mm:ss') });
   const buffer = await image.getBuffer('image/jpeg');
   return new Response(buffer, {
     headers: {
